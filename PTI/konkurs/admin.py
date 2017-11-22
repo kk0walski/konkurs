77355907d28deed3b9acf9b8c18521c94b00ed34
Karol <karol.kowalski95@gmail.com>
2017-11-22 22:09:49 +0100

    Adding models to admin

--- PTI/konkurs/admin.py
+++ PTI/konkurs/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from system.models import Author, Competition, Registration
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+class CompetitionAdmin(admin.ModelAdmin):
+    pass
+
+class RegistrationAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Competition, CompetitionAdmin)
+admin.site.register(Registration, RegistrationAdmin)
