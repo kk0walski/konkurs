f0b433ad1aee84d88e048803c5e3bf55a58c96a9
Karol <karol.kowalski95@gmail.com>
2017-11-22 23:28:25 +0100

    import static files

--- PTI/PTI/settings.py
+++ PTI/PTI/settings.py
@@ -119,4 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/karol/Dokumenty/konkurs/PTI/static'
 LOGIN_REDIRECT_URL = '/'
