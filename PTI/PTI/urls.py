ac39d838c60511f6b144f6446be2dd0ecfe7db32
karol <karol.kowalski95@gmail.com>
2017-11-29 16:48:09 +0100

    url changes

--- PTI/PTI/urls.py
+++ PTI/PTI/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls.static import static
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
