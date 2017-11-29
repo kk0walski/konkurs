634fc82a873fea467275527e3185a8b9b6ec62e6
karol <karol.kowalski95@gmail.com>
2017-11-29 20:07:18 +0100

    url fixed

--- laguna/laguna/urls.py
+++ laguna/laguna/urls.py
@@ -20,4 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^zgloszenia/', TemplateView.as_view(template_name="zgloszenie.html")),
 ]
