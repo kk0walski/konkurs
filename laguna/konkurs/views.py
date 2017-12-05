31ace035de0ec183cb735b34ef9aa3c02e03e67e
karol <karol.kowalski95@gmail.com>
2017-12-05 19:30:47 +0100

    poprawa widoku

--- laguna/konkurs/views.py
+++ laguna/konkurs/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .forms import UczestnikForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def uczestnik_new(request):
     if request.method == 'POST':
         form = UczestnikForm(request.POST)
         if form.is_valid():
-            pass
+            uczestnik = form.save(commit=False)
+            uczestnik.save()
+            return redirect('workpost', pk=uczestnik.pk)
     else:
         form = UczestnikForm()
     return render(request, 'zgloszenie.html', {'form' : form})
