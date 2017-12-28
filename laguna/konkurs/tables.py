34d416694ead2508174e55e67de7fc0e4aa948a0
karol <karol.kowalski95@gmail.com>
2017-12-28 23:36:19 +0100

    Refaktoryzacja

--- laguna/konkurs/tables.py
+++ laguna/konkurs/tables.py
@@ -1,7 +1,6 @@
 import django_tables2 as tables
 from .models import Work
 
-
 class WorkTable(tables.Table):
     details = tables.TemplateColumn('<a href="works/{{record.category}}/{{record.id}}">Show details</a>')
     class Meta:
