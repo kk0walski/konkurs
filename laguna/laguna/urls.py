244ed5baf4fd1446e5d69a5b43d3b96d9aff7fcd
kk0walski <karol.kowalski95@gmail.com>
2021-07-03 22:32:00 +0200

    Fix docs.

--- laguna/laguna/urls.py
+++ laguna/laguna/urls.py
@@ -20,9 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('konkurs.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('ratings/', include('star_ratings.urls', namespace='ratings')),
-    path('admin/doc/', include('django.contrib.admindocs.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
