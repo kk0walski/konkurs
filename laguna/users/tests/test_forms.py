b3cf205520ab1fc3abd007c667a491477489db6a
kk0walski <karol.kowalski95@gmail.com>
2021-08-11 15:10:01 +0200

    Test empty form.

--- laguna/users/tests/test_forms.py
+++ laguna/users/tests/test_forms.py
@@ -7,7 +7,6 @@ class AddUserForm(TestCase):
     def test_html(self):
         form = UserForm()
         self.assertFalse(form.is_valid())
-        #print(str(form))
         self.assertInHTML(
             '<input type="email" name="email" maxlength="254" required id="id_email">',
             str(form),
@@ -28,3 +27,14 @@ class AddUserForm(TestCase):
             '<input type="text" name="last_name" maxlength="150" id="id_last_name">',
             str(form),
         )
+
+    def test_empty_form(self):
+        form = UserForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['email'], ['This field is required.'])
+        self.assertEqual(form.errors['username'], ['This field is required.'])
+        self.assertEqual(form.errors['birthday'], ['This field is required.'])
+        self.assertEqual(form.errors['nationality'], ['This field is required.'])
+        self.assertEqual(form.errors['place_of_birth'], ['This field is required.'])
+        self.assertEqual(form.errors['password'], ['This field is required.'])
+        self.assertEqual(form.errors['password2'], ['This field is required.'])
