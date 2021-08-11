6045c492e9d934d6457f6616aef17719124532d5
kk0walski <karol.kowalski95@gmail.com>
2021-08-11 14:06:45 +0200

    Refactor tests

--- laguna/users/tests/test_templates.py
+++ laguna/users/tests/test_templates.py
@@ -5,9 +5,15 @@ from django.urls import reverse
 class RegisterTest(TestCase):
     def setUp(self):
         self.register_url = reverse("register")
+        self.index_url = reverse("index")
         return super().setUp()
 
     def test_view_page(self):
         response = self.client.get(self.register_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "registration/register.html")
+
+    def test_index(self):
+        response = self.client.get(self.index_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "index.html")
