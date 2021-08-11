3e795a4f41d7fe63872e16043419ece2a7d8465e
kk0walski <karol.kowalski95@gmail.com>
2021-08-11 11:20:13 +0200

    First steps for profile.

--- laguna/users/views.py
+++ laguna/users/views.py
@@ -3,18 +3,18 @@ from django.shortcuts import render
 
 
 from .forms import UserForm, AddressCreateForm
-from .models import CustomUser
+from .models import CustomUser, Address
 
 # Create your views here.
 class Register(CreateView):
     model = CustomUser
     form_class = UserForm
-    template_name = 'registration/register.html'
+    template_name = "accounts/register.html"
 
     def get(self, request):
         user_form = UserForm()
         address_form = AddressCreateForm()
-        context = {'user_form': user_form, 'address_form': address_form}
+        context = {"user_form": user_form, "address_form": address_form}
         return render(request, self.template_name, context)
 
     def post(self, request):
@@ -22,12 +22,20 @@ class Register(CreateView):
         address_form = AddressCreateForm(request.POST)
         if user_form.is_valid() and address_form.is_valid():
             new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
             new_address = address_form.save(commit=False)
             new_address.user = new_user
             new_address.save()
-            return render(request, 'accounts/register_done.html')
+            return render(request, "accounts/register_done.html")
 
-        context = {'user_form': user_form, 'address_form': address_form}
+        context = {"user_form": user_form, "address_form": address_form}
         return render(request, self.template_name, context)
+
+
+from django.views.generic.detail import DetailView
+
+
+class GeeksDetailView(DetailView):
+    # specify the model to use
+    model = CustomUser
