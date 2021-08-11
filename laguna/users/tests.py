a7031f91e8a7efd4c388b9203c705588852a6ff2
kk0walski <karol.kowalski95@gmail.com>
2021-08-11 13:36:09 +0200

    first test

--- laguna/users/tests.py
+++ laguna/users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class RegisterTest(TestCase):
+    def setUp(self):
+        self.register_url = reverse("register")
+        return super().setUp()
+
+    def test_view_page(self):
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/register.html")
